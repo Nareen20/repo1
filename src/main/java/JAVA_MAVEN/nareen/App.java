76918c2aa4c438fa1871f86f1b9fbf78cc7090ee
Nareen20 <nareensai8@gmail.com>
2024-12-30 18:58:10 +0530

    repo1 commited

---
+++ src/main/java/JAVA_MAVEN/nareen/App.java
@@ -0,0 +1,13 @@
+package JAVA_MAVEN.nareen;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+    	int x=10;
+    	int y=16;
+    	int z=x+y;
+        System.out.println(z);
+    }
+}
